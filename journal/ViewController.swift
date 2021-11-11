af787af8ff6b06ae42a0282d3e5b195c3ee9d7fb
LilTyland <tyland.sheard14@gmail.com>
2021-11-11 16:02:33 -0500

    Initial Commit

---
+++ journal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  journal
+//
+//  Created by Tyland on 11/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
